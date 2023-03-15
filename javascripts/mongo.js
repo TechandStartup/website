6b68523fd519f62b3de702c87a0060dc6753f050
Steve C <steve@example.com>
2023-03-15 12:42:40 -0700

    Minor fix

--- javascripts/mongo.js
+++
@@ -1,4 +0,0 @@
-const partial = document.getElementById('mongo');
-fetch('_mongo.html')
-  .then((response) => response.text())
-  .then((body) => partial.innerHTML = body);
--- javascripts/npm-run-dev.js
+++
@@ -1,4 +0,0 @@
-const partial = document.getElementById('npm-run-dev');
-fetch('_npm_run_dev.html')
-  .then((response) => response.text())
-  .then((body) => partial.innerHTML = body);
